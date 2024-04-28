77a9a9a4cba1bc274bc6e4251acefe3fa8eb6f00
Hwang Kyu Do <dodo@bemelon.me>
2024-04-28 12:47:08 +0900

    Quartz sync: Apr 28, 2024, 12:47 PM

--- quartz/plugins/filters/excalidraw.ts
+++ quartz/plugins/filters/excalidraw.ts
@@ -3,6 +3,6 @@ import { QuartzFilterPlugin } from "../types"
 export const ExcaliDrawFilter: QuartzFilterPlugin = () => ({
     name: "ExcaliDrawFilter",
     shouldPublish(_ctx, [_tree, vfile]) {
-        return vfile.path.endsWith(".excalidraw")
+        return vfile.path.endsWith(".excalidraw.md")
     },
 })
