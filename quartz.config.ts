3619c85f99838f83c8c068de614f151bde04a945
Hwang Kyu Do <dodo@bemelon.me>
2024-05-09 15:02:31 +0900

    Quartz sync: May 9, 2024, 3:02 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "bemelon.github.io/blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
